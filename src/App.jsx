ac00990ac0ea7e21a720a37c4889fc1c00252f5a
Siicked <109355725+Siicked@users.noreply.github.com>
2023-05-27 03:13:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header"
+import {ProductList} from "./components/ProductList"
+import { useState } from "react";
+
+function App() {
+
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+
+  
+  return(
+    <>
+    <Header allProducts={allProducts} setAllProducts={setAllProducts} total={total} setTotal={setTotal} countProducts={countProducts} setCountProducts={setCountProducts} />
+    <ProductList allProducts={allProducts} setAllProducts={setAllProducts} total={total} setTotal={setTotal} countProducts={countProducts} setCountProducts={setCountProducts} />
+    </>
+  )
+}
+
+
+
+export default App
